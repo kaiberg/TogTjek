fdb479258d3aab78f05bc6123d429911fa08044e
Kai Delay <kbde@riseup.net>
2023-08-15 19:39:55 +0200

    Initial Commit

---
+++ TogTjek/TogTjekApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogTjekApp.swift
+//  TogTjek
+//
+//  Created by kai delay on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TogTjekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
